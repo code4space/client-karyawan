74d7f919b785f6c65b04cffc322f003d8d74e3db
William <mauritius.william.wijaya1@gmail.com>
2023-10-12 03:37:13 +0700

    changes contant var

--- src/constant/url.js
+++ src/constant/url.js
@@ -1,2 +1,2 @@
-export const baseUrl = 'http://localhost:3001'
-export const baseUrl1 = 'http://localhost:3002'
+export const baseUrl = 'https://server-user-test.onrender.com'
+export const baseUrl1 = 'https://server-logger-test.onrender.com'
